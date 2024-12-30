582f67fccd93161da1d4dfa74d874af3e00e5f8b
Misbahul Muttaqin <misbahulmuttaqin395@gmail.com>
2024-12-30 14:30:12 +0700

    ending project astro blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), mdx()],
+  output:'server',
+  adapter: netlify(),
+});
